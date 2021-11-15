83dbc6fb8388f819c2ea06fff25fa541466a8e3d
phananh1310 <phananh13102001@gmail.com>
2021-11-15 23:16:38 +0700

    Update InterbankInterface.java

--- Detailed Design/Class Design/AIMS/src/subsystem/InterbankInterface.java
+++ Detailed Design/Class Design/AIMS/src/subsystem/InterbankInterface.java
@@ -1,5 +1,42 @@
 package subsystem;
 
-public class InterbankInterface {
+import common.exception.PaymentException;
+import common.exception.UnrecognizedException;
+import entity.payment.Card;
+import entity.payment.PaymentTransaction;
 
+/**
+ * The {@code InterbankInterface} class is used to communicate with the 
+ * {@link subsystem.InterbankSubsystem InterbankSubsystem} to make transaction
+ * @author PnAnh
+ *
+ */
+
+public interface InterbankInterface {
+	
+	/**
+	 *  Pay order, and then return the payment transaction
+	 *  
+	 *  @param card   - the credit card used for payment
+	 *  @param amount - the amount to pay
+	 *  @param contents - the transaction contents
+	 *  @return {@link entity.payment.PaymentTransaction PaymentTransaction} - if the 
+	 *  		payment is successful
+	 *  @throws PaymentException if responded with a pre-defined error code
+	 *  @throws UnrecognizedException - if responded with an unknown error code or something goes wrong
+	 */
+	public abstract PaymentTransaction payOrder(Card card, int amount, String contents) 
+			throws PaymentException,UnrecognizedException;
+	/**
+	 * Refund then return payment transaction
+	 * @param card - the credit card which would be refuned to
+	 * @param amount - the amount to refund
+	 * @param contents - the transaction contents
+	 * @return {@link entity.payment.PaymentTransaction PaymentTransaction} - if the 
+	 * 			payment is successful
+	 * @throws PaymentException if responded with a pre-defined error code
+	 * @throws UnrecognizedException - if responded with an unknown error code or something goes wrong
+	 */
+	public abstract PaymentTransaction refund(Card card, int amount, String contents) 
+			throws PaymentException, UnrecognizedException;
 }
